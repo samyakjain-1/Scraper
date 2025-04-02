68d7c515e7395d5c763e4f3c98f73f06456e6517
samyak257 <sammuhere@gmail.com>
2025-04-02 14:33:10 -0500

    push

--- app.py
+++ app.py
@@ -192,4 +192,6 @@ def index():
     return render_template_string(HTML_TEMPLATE, results=results, query=query)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
